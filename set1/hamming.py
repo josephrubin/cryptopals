29ee1d5d92afa4139ec9e5030b2588df4ed98309
josephrubin <josephmeirrubin@gmail.com>
2018-06-29 12:40:54 +0300

    more changes

--- set1/hamming.py
+++ set1/hamming.py
@@ -6,14 +6,14 @@ import sys
 
 def compute(a, b):
     if len(a) < len(b):
-        a += ('\0' * (len(b) - len(a)))
+        a += (b'\0' * (len(b) - len(a)))
     elif len(b) < len(a):
-        b += ('\0' * (len(a) - len(b)))
+        b += (b'\0' * (len(a) - len(b)))
 
     dist = 0
     for c_a, c_b in zip(iter(a), iter(b)):
-        s_a = bin(ord(c_a))[2:].zfill(8)
-        s_b = bin(ord(c_b))[2:].zfill(8)
+        s_a = bin((c_a))[2:].zfill(8)
+        s_b = bin((c_b))[2:].zfill(8)
         for bit_a, bit_b in zip(iter(s_a), iter(s_b)):
             if bit_a != bit_b:
                 dist += 1
@@ -23,6 +23,6 @@ def compute(a, b):
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
-        sys.stderr.write("first two args msut be strings to hamming dist\n")
+        sys.stderr.write("first two args must be strings to hamming dist\n")
         exit(1)
-    print(compute(sys.argv[1], sys.argv[2]))
+    print(compute(sys.argv[1].encode('ascii'), sys.argv[2].encode('ascii')))
